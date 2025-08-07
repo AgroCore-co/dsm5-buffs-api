f129bb1d80ab1d39a4c8813e5fc491c6f3ecc10e
João Barreto <joaobarretoprof@gmail.com>
2025-08-07 11:30:16 -0300

    feat: Adiciona configuração do banco no projeto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    // 1. Módulo de Configuração para carregar variáveis de ambiente (.env)
+    ConfigModule.forRoot({
+      isGlobal: true, // Torna as variáveis de ambiente disponíveis globalmente
+    }),
+    // 2. Módulo de Conexão com o Banco de Dados (PostgreSQL)
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [__dirname + '/../**/*.entity{.ts,.js}'], // Onde encontrar as entidades
+        synchronize: true, // ATENÇÃO: true apenas para desenvolvimento!
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
