ad247898d6e005da36d6c9a302da2edc1c277153
João Barreto <joaobarretoprof@gmail.com>
2025-08-22 19:40:21 -0300

    chore: Adiciona o dado microchip no DTO de búfalos

--- src/modules/rebanho/bufalo/dto/create-bufalo.dto.ts
+++ src/modules/rebanho/bufalo/dto/create-bufalo.dto.ts
@@ -32,18 +32,26 @@ export class CreateBufaloDto {
   @MaxLength(10)
   brinco?: string;
 
+  @ApiProperty({
+    description: 'Código de identificação do microchip do búfalo.',
+    example: '982000444998877',
+    required: false,
+    maxLength: 30,
+  })
+  @IsString()
+  @IsOptional()
+  @MaxLength(30)
+  microchip?: string;
+
   @ApiProperty({ description: 'Data de nascimento do búfalo.', example: '2023-05-20T00:00:00.000Z', required: false })
-  // Esta parte está correta: @Type transforma a string do JSON em um objeto Date,
-  // e @IsDate valida se a transformação resultou em uma data válida.
   @Type(() => Date)
   @IsDate()
   @IsOptional()
   dt_nascimento?: Date;
 
-  // Trocado IsString por IsEnum para garantir que apenas os valores permitidos sejam aceitos.
   @ApiProperty({
     description: 'Nível de maturidade (B-Bezerro, N-Novilho/Novilha, V-Vaca, T-Touro).',
-    enum: NivelMaturidade, // Isso informa ao Swagger quais são os valores possíveis.
+    enum: NivelMaturidade,
     example: NivelMaturidade.NOVILHO_NOVILHA,
     required: false,
   })
@@ -51,7 +59,6 @@ export class CreateBufaloDto {
   @IsOptional()
   nivel_maturidade?: NivelMaturidade;
 
-  // Trocado IsString por IsEnum para o campo 'sexo'.
   @ApiProperty({
     description: 'Sexo do búfalo (M ou F).',
     enum: SexoBufalo,
@@ -61,7 +68,6 @@ export class CreateBufaloDto {
   @IsNotEmpty()
   sexo: SexoBufalo;
 
-  // Adicionado @IsPositive para garantir que o ID seja um número inteiro positivo.
   @ApiProperty({ description: 'ID da raça do búfalo.', example: 1 })
   @IsInt()
   @IsPositive()
@@ -95,6 +101,5 @@ export class CreateBufaloDto {
   @ApiProperty({ description: 'Status do búfalo (true para ativo, false para inativo).', example: true, default: true })
   @IsBoolean()
   @IsOptional()
-  // CORREÇÃO: A propriedade deve ser opcional no tipo (`?`) se o decorator `@IsOptional` for usado.
   status?: boolean = true;
 }
