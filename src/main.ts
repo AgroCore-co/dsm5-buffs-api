55341aecb30b1e37b29d37ed03cfd051474349aa
João Barreto <joaobarretoprof@gmail.com>
2025-09-03 19:46:08 -0300

    fix: Faz correções no uso de variaveis de ambiente e atualiza o exemplo

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+    dotenv.config();
 
   const swaggerDescription = `
   Documentação da API para o sistema de gerenciamento de búfalos (BUFFS).
