4f3e68b8ef3090afbc7f78f8f9ff984a56db95ec
João Barreto <joaobarretoprof@gmail.com>
2025-09-12 19:52:50 -0300

    feat: Adiciona o submódulo de industria no módulo principal de produção

--- src/modules/producao/producao.module.ts
+++ src/modules/producao/producao.module.ts
@@ -3,8 +3,9 @@ import { ControleLeiteiroModule } from './controle-leiteiro/controle-leiteiro.mo
 import { EstoqueLeiteModule } from './estoque-leite/estoque-leite.module';
 import { ColetaModule } from './coleta/coleta.module';
 import { CicloLactacaoModule } from './ciclo-lactacao/ciclo-lactacao.module';
+import { IndustriaModule } from './industria/industria.module';
 
 @Module({
-  imports: [ControleLeiteiroModule, EstoqueLeiteModule, ColetaModule, CicloLactacaoModule],
+  imports: [ControleLeiteiroModule, EstoqueLeiteModule, ColetaModule, CicloLactacaoModule, IndustriaModule],
 })
 export class ProducaoModule {}
