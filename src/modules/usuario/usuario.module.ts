1fc4cf1bd245b4ee5dd7b287fb44def5c1130584
João Barreto <joaobarretoprof@gmail.com>
2025-09-16 20:09:52 -0300

    fix: Corrige erro no usuario module

--- src/modules/usuario/usuario.module.ts
+++ src/modules/usuario/usuario.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
 import { SupabaseModule } from '../../core/supabase/supabase.module';
+import { CoreModule } from 'src/core/core.module';
 
 @Module({
-  imports: [SupabaseModule],
+  imports: [SupabaseModule, CoreModule],
   controllers: [UsuarioController],
   providers: [UsuarioService],
 })
