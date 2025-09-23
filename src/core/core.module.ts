ff51202d563831e43a76cb25f7644fd54c9d9dc6
João Barreto <joaobarretoprof@gmail.com>
2025-09-23 17:42:40 -0300

    refactor: Inicia a configuração do ORM Prisma ao projeto

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -4,6 +4,8 @@ import { LoggerModule } from './logger/logger.module';
 import { SupabaseModule } from './supabase/supabase.module';
 import { CacheConfigModule } from './cache/cache.module';
 import { CacheService } from './cache/cache.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
@@ -11,12 +13,14 @@ import { CacheService } from './cache/cache.service';
     LoggerModule,
     SupabaseModule,
     CacheConfigModule,
+    PrismaModule
   ],
   providers: [CacheService],
   exports: [
     GeminiModule,
     LoggerModule,
     SupabaseModule,
+    PrismaService,
     CacheConfigModule,
     CacheService,
   ],
